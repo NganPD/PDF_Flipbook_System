f507d58ee55017608f86ad50257af173f572551c
PhamDinhNgan <169060597+NganPD@users.noreply.github.com>
2024-09-17 10:23:57 +0700

    Add new project

---
+++ PdfFlipbookSystem/PdfFlipbookApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PdfFlipbookApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
